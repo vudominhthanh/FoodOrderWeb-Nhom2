9c443831628656aa53a351559fc090281b2560e9
Thành Vũ <tawoz131105@gmail.com>
2025-08-20 15:44:24 +0700

    tao xonng nhanh login

--- FoodOrderWeb/FoodOrderWeb/obj/Debug/net8.0/FoodOrderWeb.AssemblyInfo.cs
+++ FoodOrderWeb/FoodOrderWeb/obj/Debug/net8.0/FoodOrderWeb.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodOrderWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+734124112338ee5401de19bd3fecf109abb1dfdf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1afb75853da0505b53c3cfc42f36971dee31bf54")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodOrderWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodOrderWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
